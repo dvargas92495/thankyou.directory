59f1bada3f4b30e230da1fee1adbe24eef95a77b
David Vargas <dvargas92495@gmail.com>
2021-08-23 18:59:42 -0500

    redeploy fe

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import React from "react";
 const Home: React.FunctionComponent = () => (
   <div>
     <h1>Welcome to the Thank You Directory!</h1>
+    <h3>
+      A database of people who have supported others and a suite of tools to
+      thank them!
+    </h3>
   </div>
 );
 
