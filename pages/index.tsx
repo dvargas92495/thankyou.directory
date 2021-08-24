66d111aab0013930063ce97892d51621bfae69c8
David Vargas <dvargas92495@gmail.com>
2021-08-23 20:53:12 -0500

    redeploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Home: React.FunctionComponent = () => (
-  <main style={{ marginTop: 64, textAlign: 'center' }}>
+  <main style={{ marginTop: 64, textAlign: "center" }}>
     <h1>Welcome to the Thank You Directory!</h1>
     <h3>
       A database of people who have supported others and a suite of tools to
       thank them!
     </h3>
-    <button style={{padding: 8, borderRadius: 8}}>Get Started!</button>
+    <button style={{ padding: 8, borderRadius: 8 }}>Get Started!</button>
   </main>
 );
 
