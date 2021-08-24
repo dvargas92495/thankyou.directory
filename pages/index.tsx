20584d61efc3e2946f2ab257790df530dacfe551
David Vargas <dvargas92495@gmail.com>
2021-08-23 19:32:49 -0500

    path not defined

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FunctionComponent = () => (
-  <main style={{ marginTop: 64 }}>
+  <main style={{ marginTop: 64, textAlign: 'center' }}>
     <h1>Welcome to the Thank You Directory!</h1>
     <h3>
       A database of people who have supported others and a suite of tools to
