841dd0b5c0e488a0bf433b1654e10770a5c78406
David Vargas <dvargas92495@gmail.com>
2021-08-25 20:29:44 -0500

    test save exact

--- a.js
+++
@@ -1,4 +0,0 @@
-const aws = require('aws-sdk');
-console.log(aws.config.credentials.accessKeyId.slice(-2));
-console.log(aws.config.credentials.secretAccessKey.slice(-2));
-console.log(aws.config.region);
