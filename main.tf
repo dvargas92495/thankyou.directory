591559ac2dc535bf1c4f7c135d9b9e458531aa2c
David Vargas <dvargas92495@gmail.com>
2021-10-05 22:57:50 -0400

    aws-clerk 1.0.1

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ module "aws-serverless-backend" {
 
 module "aws_clerk" {
   source   = "dvargas92495/clerk/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 
   zone_id  = module.aws_static_site.route53_zone_id
   clerk_id = "auitknq6b0p7"
